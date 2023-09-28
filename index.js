8013c31323a31ef38c67add0362a025e3d385a31
Hamza Yaseen <hamzza.yaseen@cooperativecomputing.com>
2023-09-28 18:51:50 +0500

    feat: design schema

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import reviewRoute from "./routes/review.js";
 import bookingRoute from "./routes/booking.js";
 import appointmentsRoute from "./routes/appointment.js"
 import emailRoute from "./routes/email.js"
+import contactRoute from "./routes/contact.js"
 
 dotenv.config();
 
@@ -50,10 +51,11 @@ app.use("/api/v1/users", userRoute);
 app.use("/api/v1/doctors", doctorRoute);
 app.use("/api/v1/reviews", reviewRoute);
 app.use("/api/v1/bookings", bookingRoute);
-app.use('/api/v1/doctorappointments', appointmentsRoute)
-app.use('/api/v1/sendemail', emailRoute)
+app.use('/api/v1/doctorappointments', appointmentsRoute);
+app.use('/api/v1/sendemail', emailRoute);
+app.use('/api/v1/contact', contactRoute)
 
 app.listen(port, () => {
-  connectDB();
+  connectDB(); 
   console.log("server listening on port" + port);
 });
